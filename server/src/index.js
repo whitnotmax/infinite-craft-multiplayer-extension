30f1c0aa36d3276fd5b63e5e51c0b2f9ebc2ddea
whitnotmax <91440193+whitnotmax@users.noreply.github.com>
2024-03-06 22:57:13 -0500

    https support

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const ws = require('ws');
-const cors = require('cors') 
+const cors = require('cors');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const {connectionHandler, closeHandler } = require('./sockets/socketHandlers');
 const newGameController = require('./controllers/newGameController');
 const games = []; 
@@ -29,13 +32,39 @@ wsServer.on('connection', socket => {
     });
 });
 
-    
-const server = app.listen(80, () => {
-    console.log("Ready");
+const httpServer = http.createServer(app);
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
 });
 
-server.on('upgrade', (request, socket, head) => {
+httpServer.on('upgrade', (request, socket, head) => {
     wsServer.handleUpgrade(request, socket, head, socket => {
       wsServer.emit('connection', socket, request);
     });
 });
+
+if (process.env.PRIVATE_KEY && process.env.CERT && process.env.CA) {
+    const privateKey = fs.readFileSync(process.env.PRIVATE_KEY);
+    const certificate = fs.readFileSync(process.env.CERT);
+    const ca = fs.readFileSync(process.env.CA);
+
+    const credentials = {
+        cert: certificate,
+        key: privateKey,
+        "ca": ca
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(443, () => {
+        console.log("HTTPS server running on port 443");
+    });
+
+    httpsServer.on('upgrade', (request, socket, head) => {
+        wsServer.handleUpgrade(request, socket, head, socket => {
+          wsServer.emit('connection', socket, request);
+        });
+    });
+} else {
+    console.log("SSL certificate not set up - if you are trying to host somewhere other than localhost the browser will refuse to connect!")
+}
